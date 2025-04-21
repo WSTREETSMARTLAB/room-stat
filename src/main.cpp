a7eb0babe1674a0b7b748121811635dbf812c914
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-04-21 22:35:22 +0300

    added comment step operations

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  // create wi-fi access point
+  // save tool auth and wi-fi credentials
+  // 
+  // connect to wi-fi with saved credentials
+  // check server status
+  //
+  // if status = ok - login user
+  // use token to auth requests
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // get sensor params
+  // format json
+  // send request to transmit api
 }
