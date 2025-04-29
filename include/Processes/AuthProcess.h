c4b100ab8171e27abf39011ca291db40ca0a94dd
martynovdmytro <dmytro_martynov@icloud.com>
2025-04-29 16:09:55 +0300

    refactoring - ToolConfig removed from App.cpp , preferences passed to AuthProcess instead

--- include/Processes/AuthProcess.h
+++ include/Processes/AuthProcess.h
@@ -2,15 +2,17 @@
 #include <Services/ApiService.h>
 #include <DTO/ToolConfig.h>
 #include <Services/DisplayService.h>
+#include <Storage/ToolPreferences.h>
 
 class AuthProcess
 {
 private:
     ApiService& apiService;
+    ToolPreferences& preferences;
     ToolConfig config;
     DisplayService& display;
     const String endpoint;
 public:
-    AuthProcess(ApiService& apiService, const ToolConfig config, DisplayService& display);
+    AuthProcess(ApiService& apiService, ToolPreferences& preferences, DisplayService& display);
     void handle();
 };
