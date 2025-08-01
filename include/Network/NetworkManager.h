f3155da468057e6bd5ab4d634f2b8c6a4c3ad39e
martynovdmytro <dmytro_martynov@icloud.com>
2025-08-01 17:35:29 +0300

    ss

--- include/Network/NetworkManager.h
+++ include/Network/NetworkManager.h
@@ -1,1 +1,26 @@
+#pragma once
+#include <WiFi.h>
+#include <Enum/NetworkState.h>
+
+class NetworkManager
+{
+private:
+    NetworkState state;
+    unsigned long stateStart;
+    unsigned long lastCheck;
+
+    const unsigned long CONNECTION_TIMEOUT = 15000;
+    const unsigned long AP_TIMEOUT = 300000;
+    const unsigned long CHECK_INTERVAL = 5000;
+    const unsigned long RECONNECT_TIMEOUT = 30000;
+    const int MAX_RECONNECTION_ATTEMPTS = 3;
+public:
+    NetworkManager();
+    void update();
+    void attemptConnection(const String& ssid, const String& password);
+    void startAP();
+    void forceReconnection();
+    void reset();
+};
+
 
