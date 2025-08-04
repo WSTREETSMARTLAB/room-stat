e225b66c79b7fde8ddb740d6f9fa3f0e670948bb
martynovdmytro <dmytro_martynov@icloud.com>
2025-08-04 09:30:55 +0300

    add DeviceState enum

--- src/App/State.cpp
+++ src/App/State.cpp
@@ -3,3 +3,6 @@
 String serverUrl = "https://wstreet.systems";
 bool serverAlive = false;
 String token = "";
+DeviceState currentDeviceState = ACTIVE;
+unsigned long lastActivityTime = 0;
+unsigned long sleepModeStartTime = 0;
